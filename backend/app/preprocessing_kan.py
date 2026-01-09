3229b7bbbfd205ba893a9c13ee7e2041b84bbe36
jppluas <jppluas@espol.edu.ec>
2026-01-08 23:52:32 -0500

    UPDATE kan prepro

--- backend/app/preprocessing_kan.py
+++ backend/app/preprocessing_kan.py
@@ -7,13 +7,9 @@ from torchvision import transforms
 # Transformaciones EXACTAS usadas en entrenamiento
 transform_kan = transforms.Compose(
     [
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
-        ),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
 
